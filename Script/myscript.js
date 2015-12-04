f7e8cb01a4b4dc4bf9e07cb77b474edc98021924
Nitin Kumar Singh <Nitin.Singh@ATMECS.CORP>
2015-12-04 18:23:28 +0530

    ng-repeat directive like for-each loop in c# displaying a list of objects

--- Script/myscript.js
+++ Script/myscript.js
@@ -5,7 +5,14 @@ var myController = function($scope){
         name:"India",
                 capital:"New Delhi",
                 flag:"/Images/indian-flag.png"};
+    var employees=[
+        {firstName:"Abhijit",lastName:"Galphat",gender:"Male",salary:"55000"},
+        {firstName:"Amol",lastName:"pawal",gender:"Male",salary:"69000"},
+        {firstName:"Nilesh",lastName:"xyz",gender:"Male",salary:"56000"},
+        {firstName:"Maduri",lastName:"Dixit",gender:"Female",salary:"85000"}
+    ];
     $scope.country=country;
+    $scope.employees=employees;
     $scope.message="1st Text Displayed by Angular JS";
 };
 myApp.controller("myController",myController);
