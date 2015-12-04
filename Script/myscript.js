4e174c091f93b35b1aa196011c5521fccaedada9
Nitin Kumar Singh <Nitin.Singh@ATMECS.CORP>
2015-12-04 16:38:06 +0530

    code with Complex Object and Directive ng-src in <IMG> tag

--- Script/myscript.js
+++ Script/myscript.js
@@ -1,7 +1,11 @@
-var myApp= angular.module("myModule",[]);
+var myApp = angular.module("myModule" ,[]);
 
 var myController = function($scope){
-    
+    var country={ 
+        name:"India",
+                capital:"New Delhi",
+                flag:"/Images/indian-flag.png"};
+    $scope.country=country;
     $scope.message="1st Text Displayed by Angular JS";
 };
 myApp.controller("myController",myController);
