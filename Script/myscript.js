794bd364de969ebb2a242b7fdacb23f982cd1bc0
Nitin Kumar Singh <Nitin.Singh@ATMECS.CORP>
2015-12-07 14:31:05 +0530

    Sort Row by table Header in angular JS using filters

--- Script/myscript.js
+++ Script/myscript.js
@@ -1,62 +1,27 @@
 var myApp = angular.module("myModule" ,[]);
 
 var myController = function($scope){
-    var country=[{ 
-        name:"India",
-                capital:"New Delhi",
-        cities:[
-            
-            {
-                name:"Hyderabad"
-            },
-            {
-                name:"Pune"
-            },
-            {
-                name:"Mumbai"
-            }
-        ]},
-                 
-                 {name:"USA",
-                capital:"Washington D.C",
-        cities:[
-            
-            {
-                name:"New York"
-            },
-            {
-                name:"Dallas"
-            },
-            {
-                name:"Las Vegas"
-            }
-        ]
-            },
-     
-                 {name:"UK",
-                capital:"London",
-        cities:[
-            
-            {
-                name:"Menchester"
-            },
-            {
-                name:"cardiff"
-            },
-            {
-                name:"Edinburgh"
-            }
-        ]
-}];
+
     var employees=[
         {firstName:"Abhijit",lastName:"Galphat",gender:"Male",salary:"55000"},
         {firstName:"Amol",lastName:"pawal",gender:"Male",salary:"69000"},
         {firstName:"Nilesh",lastName:"xyz",gender:"Male",salary:"56000"},
         {firstName:"Maduri",lastName:"Dixit",gender:"Female",salary:"85000"}
     ];
-    $scope.country=country;
+
     $scope.employees=employees;
     $scope.message="1st Text Displayed by Angular JS";
     $scope.sortColumn="firstName";
+    $scope.revSort=true;
+    $scope.sortData= function(column){
+        $scope.revSort=($scope.sortColumn==column) ? !$scope.revSort : false;
+        $scope.sortColumn=column;
+    }
+    $scope.getSortClass=function(column){
+        if($scope.sortColumn==column){
+            return $scope.revSort ? 'arrow-down' : 'arrow-up'
+        }
+        return '';
+    }
 };
 myApp.controller("myController",myController);
